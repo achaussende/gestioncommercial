48b4e0b16c4cba5b14eceeec145389f1cf62864d
antoine395.caron@laposte.net <antoine395.caron@laposte.net@0490055f-50a4-f054-b168-b05a7217a90c>
2014-01-04 14:17:08 +0000

    Fin Detail_cde

--- trunk/Gestion_Commerciale/Metier/Detail_Cde.cs
+++ trunk/Gestion_Commerciale/Metier/Detail_Cde.cs
@@ -12,6 +12,27 @@ namespace Metier
         private String qte_cde;
         private String livree;
 
+        public String Livree
+        {
+            get { return livree; }
+            set { livree = value; }
+        }
+        public String No_command
+        {
+            get { return no_command; }
+            set { no_command = value; }
+        }
+        public String No_article
+        {
+            get { return no_article; }
+            set { no_article = value; }
+        }
+        public String Qte_cde
+        {
+            get { return qte_cde; }
+            set { qte_cde = value; }
+        }
+
         public Detail_Cde(int hashCode, String no_command, String no_article, String qte_cde, String livree)
         {
             this.no_command = no_command;
