be7790cf376d046987eee170a602ec4ab8561642
antoine395.caron@laposte.net <antoine395.caron@laposte.net@0490055f-50a4-f054-b168-b05a7217a90c>
2014-01-04 14:24:41 +0000

    update fonction

--- trunk/Gestion_Commerciale/Utilitaires/Fonction.cs
+++ trunk/Gestion_Commerciale/Utilitaires/Fonction.cs
@@ -7,5 +7,34 @@ namespace Utilitaires
 {
     class Fonction
     {
+                /// <summary>
+        /// Convertir une chaine date de mysql en datetime
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static DateTime StringToDate(String dt)
+        {
+        String[] coupehr = dt.Split(' ');
+        String[] coupedt = coupehr[0].Split('/');
+        return new DateTime(int.Parse(coupedt[2]), int.Parse(coupedt[1]),
+        int.Parse(coupedt[0]));
+        }
+        /// <summary>
+        /// Convertir une datetime en chaine pour mysql
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static String DateToString(DateTime dt)
+        {
+            String retour = "";
+                        retour += dt.Year + "-";
+            retour += dt.Month + "-";
+            retour += dt.Day + " ";
+            retour += dt.Hour + ":";
+            retour += dt.Minute + ":";
+            retour += dt.Second;
+            return retour;
+        }
+        
     }
 }
