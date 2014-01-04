7188d36ca92fa57271958aa05da11c180111724a
antoine395.caron@laposte.net <antoine395.caron@laposte.net@0490055f-50a4-f054-b168-b05a7217a90c>
2014-01-04 14:44:30 +0000

    ouverture et fermeture ??

---
+++ trunk/Gestion_Commerciale/Metier/FermetureApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistance;
+using MySql.Data.MySqlClient;
+using MesErreurs;
+
+namespace Metier
+{
+    public class FermetureApplication
+    {
+        /// <summary>
+        /// Tentative de connexion à la base
+        /// </summary>
+        /// <returns></returns>
+        static public bool getFermeture()
+        {
+            try
+            {
+                //A revérifer
+                Connexion macnx = Connexion.getInstance();
+                MySqlConnection mysqlcnx = Connexion.getInstance().getConnexion();
+                macnx.closeconnexion();
+                return true;
+            }
+            catch (MonException excep)
+            {
+                throw excep;
+            }
+        }
+    }
+}
