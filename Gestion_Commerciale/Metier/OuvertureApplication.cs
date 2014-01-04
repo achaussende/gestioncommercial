e82009db0a42232c6cca95b8aad72cfd29d49660
Antoine CARON <Slashgear@users.noreply.github.com>
2014-01-04 14:44:30 +0000

    ouverture et fermeture ??

---
+++ Gestion_Commerciale/Metier/OuvertureApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistance;
+using MySql.Data.MySqlClient;
+using MesErreurs;
+
+namespace Metier
+{
+    public class OuvertureApplication
+    {
+        /// <summary>
+        /// Tentative de connexion à la base
+        /// </summary>
+        /// <returns></returns>
+          static public bool getOuverture()
+            {
+                try
+            {
+                    //A revérifer
+                Connexion macnx = Connexion.getInstance();
+                MySqlConnection mysqlcnx = Connexion.getInstance().getConnexion();
+                return true;
+            }
+            catch (MonException excep)
+            {
+            throw excep;
+            }
+            }
+    }
+}
